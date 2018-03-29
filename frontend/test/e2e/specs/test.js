c384c39622ab9edb198adc5e4c03f0efdda8e672
Giuseppe Tufo <pepj2000@gmail.com>
2018-03-29 18:04:17 +0200

    e2e tests

--- frontend/test/e2e/specs/test.js
+++ frontend/test/e2e/specs/test.js
@@ -1,8 +1,10 @@
 // For authoring Nightwatch tests, see
 // http://nightwatchjs.org/guide#usage
 
+// require('../../../../bin/www');
+
 module.exports = {
-  'default e2e tests': function test(browser) {
+  'Log In': (browser) => {
     // automatically uses dev Server port from /config.index.js
     // default: http://localhost:8080
     // see nightwatch.conf.js
@@ -10,10 +12,31 @@ module.exports = {
 
     browser
       .url(devServer)
-      .waitForElementVisible('.MainContainer', 5000)
-      // .assert.elementPresent('.hello')
-      // .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      // .assert.elementCount('img', 1)
-      .end();
+      .pause(1000);
+
+    browser.expect.element('body').to.be.present.before(1000);
+    browser.expect.element('.MainContainer').to.be.present.before(5000);
+    browser.expect.element('.Backdrop').to.have.attribute('class').which.not.contains('in');
+    browser.expect.element('.Modal').to.have.attribute('class').which.not.contains('in');
+    // eslint-disable-next-line
+    browser.expect.element('.TopBar').to.be.present;
+    browser.expect.element('.Title').text.to.contain('Assist 2');
+    // eslint-disable-next-line
+    browser.expect.element('.BodyWrapper').to.be.present;
+    // eslint-disable-next-line
+    browser.expect.element('.HomeContainer').to.be.present;
+    browser.click('.SignInButton');
+    browser.expect.element('.Backdrop').to.have.attribute('class').which.contains('in');
+    browser.expect.element('.Modal').to.have.attribute('class').which.contains('in');
+    browser.pause(1000);
+    browser.setValue('input[type=text]', 'testuser');
+    browser.setValue('input[type=password]', 'testBBcode');
+    browser.click('.Button:first-of-type');
+    browser.pause(3000);
+    // eslint-disable-next-line
+    browser.expect.element('.HomeContainer').not.to.be.present;
+    // eslint-disable-next-line
+    browser.expect.element('.PageContainer').to.be.present;
+    browser.end();
   },
 };
